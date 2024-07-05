b4c942452f2c5189a72dded85a24eec694e88cdf
alisonlai <mr.lai.who@gmail.com>
2024-07-05 16:11:48 +0800

    feat: undo-tree 临时文件修改位置

--- lisp/init-undo-tree-local.el
+++ lisp/init-undo-tree-local.el
@@ -3,6 +3,8 @@
 ;;; Code:
 
 (require-package 'undo-tree)
+(setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
+(setq undo-tree-auto-save-history t)
 (global-undo-tree-mode)
 (provide 'init-undo-tree-local)
 ;;; init-undo-tree-local.el ends here
